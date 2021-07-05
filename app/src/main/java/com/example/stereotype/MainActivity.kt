2cfbfa12d7411b551feb8cbc48a25083f725e517
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-07-05 19:45:08 +0530

    theme correction

--- app/src/main/java/com/example/stereotype/MainActivity.kt
+++ app/src/main/java/com/example/stereotype/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.stereotype
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.stereotype.databinding.ActivityMainBinding
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     lateinit private var binding : ActivityMainBinding
@@ -14,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         setSupportActionBar(binding.toolbar)
+
 
     }
 }
